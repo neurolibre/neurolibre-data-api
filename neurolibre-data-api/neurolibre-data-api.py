3f446e6612d8cec77bfa4b9e37dcd5b15c845de1
Loic Tetrel <37963074+ltetrel@users.noreply.github.com>
2021-04-20 13:17:24 -0400

    Update neurolibre-data-api.py
    removing lockfile before abort

--- neurolibre-data-api/neurolibre-data-api.py
+++ neurolibre-data-api/neurolibre-data-api.py
@@ -116,12 +116,13 @@ def api_books_post(user):
             if dict_log["phase"] == "ready":
                 commit_hash = dict_log["binder_ref_url"].split("/")[-1]
             else:
+                os.remove(lock_filepath)
                 flask.abort(500, "environment not ready!")
     if commit_hash == None:
+        os.remove(lock_filepath)
         flask.abort(500, "commit hash not found from built environment!")
     results = book_get_by_params(commit_hash=commit_hash)
     os.remove(lock_filepath)
-    
     if not results:
         flask.abort(424)
 
