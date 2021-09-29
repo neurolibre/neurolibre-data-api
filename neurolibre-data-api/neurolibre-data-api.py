6e8613c0bafed8b09b7f97c2b740a3801509c039
Loic Tetrel <37963074+ltetrel@users.noreply.github.com>
2021-09-29 12:49:53 -0400

    Update neurolibre-data-api.py
    Adding notebook list

--- neurolibre-data-api/neurolibre-data-api.py
+++ neurolibre-data-api/neurolibre-data-api.py
@@ -24,21 +24,30 @@ htpasswd = HtPasswdAuth(app)
 
 def load_all(globpath=BOOK_PATHS):
     book_collection = []
-    
+
     paths = glob.glob(globpath)
     for path in paths:
-        path_list = path.replace(".tar.gz", "").split("/")
+        curr_dir = path.replace(".tar.gz", "")
+        path_list = curr_dir.split("/")
         commit_hash = path_list[-1]
         repo = path_list[-2]
         provider = path_list[-3]
         user = path_list[-4]
+        nb_list = []
+        for (dirpath, dirnames, filenames) in os.walk(curr_dir + "/_build/jupyter_execute"):
+            for input_file in filenames:
+                if input_file.split(".")[-1] == "ipynb":
+                    nb_list += [os.path.join(dirpath, input_file).replace("/DATA/book-artifacts", BOOK_URL)]
+        nb_list = sorted(nb_list)
         book_dict = {"book_url": BOOK_URL + f"/{user}/{provider}/{repo}/{commit_hash}/_build/html/"
+                     , "book_build_logs": BOOK_URL + f"/{user}/{provider}/{repo}/{commit_hash}/book-build.log"
                      , "download_link": BOOK_URL + path.replace("/DATA/book-artifacts", "")
+                     , "notebook_list": nb_list
                      , "repo_link": f"https://{provider}/{user}/{repo}"
                      , "user_name": user
                      , "repo_name": repo
                      , "provider_name": provider
-                     , "commit_hash": commit_hash 
+                     , "commit_hash": commit_hash
                      , "time_added": time.ctime(os.path.getctime(path))}
         book_collection += [book_dict]
 
